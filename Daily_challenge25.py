23d3fad1f105e178b1f6194fc2e5a2019d167259
Belvah <84146898+Belvah@users.noreply.github.com>
2026-04-01 17:46:36 +0300

    Delete Daily_challenge25.py file

--- Daily_challenge25.py
+++
@@ -1,8 +0,0 @@
-# Write a program that opens a text file and iterates through each line using a for loop, printing each line separately.
-
-file = open("mbox.txt", "r")
-
-for line in file:
-    print(line.strip())
-
-file.close()
